509ee49e26fe0bbb8377f8ee29835f68ba32b46c
jyri <soppela.jyri@gmail.com>
2019-12-09 14:40:10 +0200

    change nosetests call

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -e
 set -ex
 
-nosetests --ckan \
-          --nologcapture \
-          --with-pylons=subdir/test.ini \
-          --with-coverage \
-          --cover-package=ckanext.issues \
-          --cover-inclusive \
-          --cover-erase \
-          --cover-tests \
-          ckanext/issues
+#nosetests --ckan \
+#          --nologcapture \
+#          --with-pylons=subdir/test.ini \
+#          --with-coverage \
+#          --cover-package=ckanext.issues \
+#          --cover-inclusive \
+#          --cover-erase \
+#          --cover-tests \
+#          ckanext/issues
+
+/usr/local/bin/ckan-nosetests \
+ --ckan --with-pylons=/usr/lib/adx/ckanext-issues/test.ini /usr/lib/adx/ckanext-issues/ckanext/issues/tests
