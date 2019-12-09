6484390c528b005a1130f22b2d54e574abb1c602
jyri <soppela.jyri@gmail.com>
2019-12-09 13:52:54 +0200

    disable Flake8

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,10 +1,6 @@
 #!/bin/sh -e
 set -ex
 
-flake8 --version
-# stop the build if there are Python syntax errors or undefined names
-flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics --exclude ckan,{{ project }}
-
 nosetests --ckan \
           --nologcapture \
           --with-pylons=subdir/test.ini \
@@ -14,6 +10,3 @@ nosetests --ckan \
           --cover-erase \
           --cover-tests \
           ckanext/issues
-
-# strict linting
-flake8 . --count --max-complexity=10 --max-line-length=127 --statistics --exclude ckan,{{ project }}
