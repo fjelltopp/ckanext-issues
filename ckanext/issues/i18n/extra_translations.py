69399eff7ec8a0f0c6f2e29612f7fd461867e901
Jonathan Berry <jonathan.s.berry@googlemail.com>
2020-01-08 17:31:37 +0000

    Extra translations.

---
+++ ckanext/issues/i18n/extra_translations.py
@@ -0,0 +1,18 @@
+from ckan.common import _
+
+# Extra strings for translation not specified in the python or javascript code
+# Most of them come from json configuration files, or the DB.
+extra_translations = [
+    _('Newest'),
+    _('Oldest'),
+    _('Most Commented'),
+    _('Least Commented'),
+    _('Most Recently Updated'),
+    _('Least Recently Updated'),
+    _('Open'),
+    _('Closed'),
+    _('Visible'),
+    _('Hidden'),
+    _('Status'),
+    _('Visibility')
+]
